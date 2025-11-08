da576ba78ad0dab4a6079dc84d4a7af795cada92
Agostinho Junior <junioraw5732@gmail.com>
2025-11-08 17:30:07 +0000

    Add IronTradeClient trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+use anyhow::Result;
+
+trait IronTradeClient {
+    async fn buy_market(req: BuyMarketRequest) -> Result<BuyMarketResponse>;
+    async fn sell_market(req: SellMarketRequest) -> Result<SellMarketRequest>;
+    async fn get_orders() -> Result<GetOrdersResponse>;
+    async fn get_order(order_id: String) -> Result<GetOrderResponse>;
+    async fn get_positions() -> Result<GetPositionsResponse>;
+}
+
+struct BuyMarketRequest {}
+struct SellMarketRequest {}
+
+struct BuyMarketResponse {}
+struct SellMarketResponse {}
+struct GetOrderResponse {}
+struct GetOrdersResponse {}
+struct GetPositionsResponse {}
