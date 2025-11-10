235639bcccd9b1bbf5139a16f5dee60825f20574
Agostinho Junior <junioraw5732@gmail.com>
2025-11-10 20:19:18 +0000

    import anyhow::Result

--- src/api/client.rs
+++ src/api/client.rs
@@ -2,13 +2,14 @@
 
 use crate::api::request::{BuyMarketRequest, SellMarketRequest};
 use crate::api::response::{BuyMarketResponse, GetOpenPositionResponse, GetOrdersResponse};
+use anyhow::Result;
 
 pub trait IronTradeClient {
-    async fn buy_market(&self, req: BuyMarketRequest) -> anyhow::Result<BuyMarketResponse>;
-    async fn sell_market(&self, req: SellMarketRequest) -> anyhow::Result<SellMarketRequest>;
-    async fn get_orders(&self) -> anyhow::Result<GetOrdersResponse>;
+    async fn buy_market(&self, req: BuyMarketRequest) -> Result<BuyMarketResponse>;
+    async fn sell_market(&self, req: SellMarketRequest) -> Result<SellMarketRequest>;
+    async fn get_orders(&self) -> Result<GetOrdersResponse>;
     async fn get_open_position(
         &self,
         asset_symbol: String,
-    ) -> anyhow::Result<GetOpenPositionResponse>;
+    ) -> Result<GetOpenPositionResponse>;
 }
