b91f070ac83b247e5c5f376ca60fe552afe2892c
Agostinho Junior <junioraw5732@gmail.com>
2025-11-10 21:55:04 +0000

    Fix async in public trait warning

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use crate::api::request::{BuyMarketRequest, SellMarketRequest};
-use crate::api::response::{BuyMarketResponse, GetOpenPositionResponse, GetOrdersResponse, SellMarketResponse};
+use crate::api::response::{
+    BuyMarketResponse, GetOpenPositionResponse, GetOrdersResponse, SellMarketResponse,
+};
 use anyhow::Result;
 
 pub trait IronTradeClient {
-    async fn buy_market(&self, req: BuyMarketRequest) -> Result<BuyMarketResponse>;
-    async fn sell_market(&self, req: SellMarketRequest) -> Result<SellMarketResponse>;
-    async fn get_orders(&self) -> Result<GetOrdersResponse>;
-    async fn get_open_position(
+    fn buy_market(
+        &self,
+        req: BuyMarketRequest,
+    ) -> impl Future<Output = Result<BuyMarketResponse>> + Send;
+    fn sell_market(
+        &self,
+        req: SellMarketRequest,
+    ) -> impl Future<Output = Result<SellMarketResponse>> + Send;
+    fn get_orders(&self) -> impl Future<Output = Result<GetOrdersResponse>> + Send;
+    fn get_open_position(
         &self,
         asset_symbol: String,
-    ) -> Result<GetOpenPositionResponse>;
+    ) -> impl Future<Output = Result<GetOpenPositionResponse>> + Send;
 }
