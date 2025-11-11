ed3136454ce5ac42e462f1f1ce21af8aa5e3ec3e
Agostinho Junior <junioraw5732@gmail.com>
2025-11-11 20:02:21 +0000

    Add documentation to IronTradeClient

--- src/api/client.rs
+++ src/api/client.rs
@@ -6,16 +6,24 @@ use crate::api::response::{
 };
 use anyhow::Result;
 
+/// A trait for instances of a trading client, which allows operations with the underlying trading broker.
 pub trait IronTradeClient {
+    /// Places a market buy order. If successful returns the order id of the newly placed order.
     fn buy_market(
         &mut self,
         req: BuyMarketRequest,
     ) -> impl Future<Output = Result<BuyMarketResponse>> + Send;
+
+    /// Places a market sell order. If successful returns the order of the newly placed order.
     fn sell_market(
         &mut self,
         req: SellMarketRequest,
     ) -> impl Future<Output = Result<SellMarketResponse>> + Send;
+
+    /// Returns all placed orders regardless of status.
     fn get_orders(&self) -> impl Future<Output = Result<GetOrdersResponse>> + Send;
+
+    /// Returns an open position for the given asset_symbol.
     fn get_open_position(
         &self,
         asset_symbol: String,
