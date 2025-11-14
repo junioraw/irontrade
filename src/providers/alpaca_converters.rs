fac7f049ed5764a2a5fdf4ce40a4ed96fb49f527
Agostinho Junior <junioraw5732@gmail.com>
2025-11-14 20:06:45 +0000

    Use Self where applicable

--- src/providers/alpaca_converters.rs
+++ src/providers/alpaca_converters.rs
@@ -11,8 +11,8 @@ use apca::api::v2::position::Position;
 impl From<ApcaAmount> for Amount {
     fn from(amount: ApcaAmount) -> Self {
         match amount {
-            ApcaAmount::Quantity { quantity } => Amount::Quantity { quantity },
-            ApcaAmount::Notional { notional } => Amount::Notional { notional },
+            ApcaAmount::Quantity { quantity } => Self::Quantity { quantity },
+            ApcaAmount::Notional { notional } => Self::Notional { notional },
         }
     }
 }
@@ -20,15 +20,15 @@ impl From<ApcaAmount> for Amount {
 impl From<Amount> for ApcaAmount {
     fn from(amount: Amount) -> Self {
         match amount {
-            Amount::Quantity { quantity } => ApcaAmount::Quantity { quantity },
-            Amount::Notional { notional } => ApcaAmount::Notional { notional },
+            Amount::Quantity { quantity } => Self::Quantity { quantity },
+            Amount::Notional { notional } => Self::Notional { notional },
         }
     }
 }
 
 impl From<Position> for OpenPosition {
     fn from(position: Position) -> Self {
-        OpenPosition {
+        Self {
             asset_symbol: position.symbol.to_string(),
             average_entry_price: position.average_entry_price,
             quantity: position.quantity,
@@ -40,11 +40,11 @@ impl From<Position> for OpenPosition {
 impl From<ApcaOrderStatus> for OrderStatus {
     fn from(status: ApcaOrderStatus) -> Self {
         match status {
-            ApcaOrderStatus::New => OrderStatus::New,
-            ApcaOrderStatus::PartiallyFilled => OrderStatus::PartiallyFilled,
-            ApcaOrderStatus::Filled => OrderStatus::Filled,
-            ApcaOrderStatus::Expired => OrderStatus::Expired,
-            _ => OrderStatus::Unimplemented,
+            ApcaOrderStatus::New => Self::New,
+            ApcaOrderStatus::PartiallyFilled => Self::PartiallyFilled,
+            ApcaOrderStatus::Filled => Self::Filled,
+            ApcaOrderStatus::Expired => Self::Expired,
+            _ => Self::Unimplemented,
         }
     }
 }
@@ -52,8 +52,8 @@ impl From<ApcaOrderStatus> for OrderStatus {
 impl From<Type> for OrderType {
     fn from(type_: Type) -> Self {
         match type_ {
-            Type::Market => OrderType::Market,
-            Type::Limit => OrderType::Limit,
+            Type::Market => Self::Market,
+            Type::Limit => Self::Limit,
             _ => todo!(),
         }
     }
@@ -61,7 +61,7 @@ impl From<Type> for OrderType {
 
 impl From<ApcaOrder> for Order {
     fn from(order: ApcaOrder) -> Self {
-        Order {
+        Self {
             order_id: order.id.to_string(),
             asset_symbol: order.symbol,
             filled_quantity: order.filled_quantity,
