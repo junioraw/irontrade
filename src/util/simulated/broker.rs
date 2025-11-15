835c87b46504b197a636fe248730ba6f44df5bfb
Agostinho Junior <junioraw5732@gmail.com>
2025-11-15 22:17:37 +0000

    Rename exchange_rate to notional_per_unit
    It was confusing to reason whether the rate was quantity per notional unit or notional per quantity unit. The new name is an attempt to make things clear.
    
    Also make get_notional_unit (previously get_exchange_rate) private. No planned usages, except for the internal workings of SimulatedBroker

--- src/util/simulated/broker.rs
+++ src/util/simulated/broker.rs
@@ -13,7 +13,7 @@ pub struct SimulatedBroker {
     currency: String,
     notional_assets: HashSet<String>,
     orders: HashMap<String, FilledOrder>,
-    exchange_rates: HashMap<AssetPair, Num>,
+    notional_per_unit: HashMap<AssetPair, Num>,
     balances: HashMap<String, Num>,
 }
 
@@ -71,7 +71,7 @@ impl SimulatedBroker {
             currency: currency.into(),
             notional_assets,
             orders: HashMap::new(),
-            exchange_rates: HashMap::new(),
+            notional_per_unit: HashMap::new(),
             balances: starting_balances.clone(),
         })
     }
@@ -79,15 +79,15 @@ impl SimulatedBroker {
     // Only supports market orders,
     // in this case they execute immediately since the exchange rate is determined in this method
     pub fn place_order(&mut self, order_req: MarketOrderRequest) -> Result<String> {
-        let exchange_rate = &self.get_exchange_rate(&order_req.asset_pair)?;
+        let notional_per_unit = &self.get_notional_per_unit(&order_req.asset_pair)?;
 
         let quantity: &Num = match &order_req.amount {
             Amount::Quantity { quantity } => quantity,
-            Amount::Notional { notional } => &(notional / exchange_rate),
+            Amount::Notional { notional } => &(notional / notional_per_unit),
         };
 
         let notional: &Num = match &order_req.amount {
-            Amount::Quantity { quantity } => &(quantity * exchange_rate),
+            Amount::Quantity { quantity } => &(quantity * notional_per_unit),
             Amount::Notional { notional } => notional,
         };
 
@@ -163,17 +163,17 @@ impl SimulatedBroker {
             .unwrap_or(Num::from(0))
     }
 
-    pub fn get_exchange_rate(&self, asset_pair: &AssetPair) -> Result<Num> {
+    fn get_notional_per_unit(&self, asset_pair: &AssetPair) -> Result<Num> {
         self.check_notional(asset_pair)?;
-        self.exchange_rates
+        self.notional_per_unit
             .get(&asset_pair)
             .map(Num::clone)
             .ok_or(format_err!("Asset pair {} can't be traded", asset_pair))
     }
 
-    pub fn set_exchange_rate(&mut self, asset_pair: AssetPair, rate: Num) -> Result<()> {
+    pub fn set_notional_per_unit(&mut self, asset_pair: AssetPair, notional_per_unit: Num) -> Result<()> {
         self.check_notional(&asset_pair)?;
-        self.exchange_rates.insert(asset_pair.clone(), rate.clone());
+        self.notional_per_unit.insert(asset_pair.clone(), notional_per_unit.clone());
         Ok(())
     }
 
@@ -255,7 +255,7 @@ mod tests {
     fn place_order_no_balance() {
         let mut broker = SimulatedBrokerBuilder::new("USD").build();
 
-        let _ = broker.set_exchange_rate(
+        let _ = broker.set_notional_per_unit(
             AssetPair::from_str("GBP/USD").unwrap(),
             Num::from_str("1.31").unwrap(),
         );
@@ -291,7 +291,7 @@ mod tests {
             .set_balance(Num::from_str("14.1").unwrap())
             .build();
 
-        let _ = broker.set_exchange_rate(
+        let _ = broker.set_notional_per_unit(
             AssetPair::from_str("GBP/USD").unwrap(),
             Num::from_str("1.31").unwrap(),
         );
@@ -316,7 +316,7 @@ mod tests {
             .build();
 
         broker
-            .set_exchange_rate(
+            .set_notional_per_unit(
                 AssetPair::from_str("GBP/USD").unwrap(),
                 Num::from_str("1.31").unwrap(),
             )
@@ -342,7 +342,7 @@ mod tests {
             .build();
 
         broker
-            .set_exchange_rate(
+            .set_notional_per_unit(
                 AssetPair::from_str("GBP/USD").unwrap(),
                 Num::from_str("1.31").unwrap(),
             )
@@ -378,7 +378,7 @@ mod tests {
             .build();
 
         broker
-            .set_exchange_rate(
+            .set_notional_per_unit(
                 AssetPair::from_str("GBP/USD").unwrap(),
                 Num::from_str("1.31").unwrap(),
             )
@@ -414,7 +414,7 @@ mod tests {
             .build();
 
         let err = broker
-            .set_exchange_rate(
+            .set_notional_per_unit(
                 AssetPair::from_str("GBP/USDT").unwrap(),
                 Num::from_str("1.31").unwrap(),
             )
@@ -430,7 +430,7 @@ mod tests {
             .build();
 
         let err = broker
-            .set_exchange_rate(
+            .set_notional_per_unit(
                 AssetPair::from_str("USD/GBP").unwrap(),
                 Num::from_str("1.31").unwrap(),
             )
