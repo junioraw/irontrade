1f94618768cf3a4eba5fff6f2c1e61b13fd1e6ea
Agostinho Junior <junioraw5732@gmail.com>
2025-11-16 22:39:07 +0000

    Add documentation for common::Amount and common::AssetPair

--- src/api/common.rs
+++ src/api/common.rs
@@ -1,18 +1,34 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use num_decimal::Num;
 use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 use std::string::ParseError;
-use num_decimal::Num;
 
+/// Enum representing different value types, can either be a quantity or a notional value.
 pub enum Amount {
+    /// Quantity, usually amount of non-notional assets.
     Quantity { quantity: Num },
+    /// Notional, usually amount of a notional asset, for example a currency.
     Notional { notional: Num },
 }
 
 #[derive(Hash, PartialEq, Eq, Clone, Debug)]
+/// Struct defining an asset pair, usually used in buy/sell orders.
+///
+/// Can either be initialized via struct construction or from parsing a string representing an asset pair.
+///
+/// #Examples
+///
+/// ```
+/// use irontrade::api::common::AssetPair;
+///
+/// let asset_pair = AssetPair::from_str("BTC/USD").unwrap();
+/// ```
 pub struct AssetPair {
+    /// A notional asset, like a currency or a tethered crypto coin.
     pub notional_asset: String,
+    /// A quantifiable asset, like a stock or a crypto coin.
     pub quantity_asset: String,
 }
 
