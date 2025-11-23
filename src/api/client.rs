80700010db11bda86cc3ce3537a8de0fb117f7e5
Agostinho Junior <junioraw5732@gmail.com>
2025-11-23 14:14:32 +0000

    Add new versions of API methods

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,27 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use crate::api::request::OrderRequestV1;
+use crate::api::request::{OrderRequest, OrderRequestV1};
 use crate::api::response::{
-    GetCashResponse, GetOpenPositionResponse, GetOrdersResponseV1, OrderResponse,
+    GetCashResponse, GetOpenPositionResponse, GetOrdersResponse, GetOrdersResponseV1, OrderResponse,
 };
 use anyhow::Result;
 
 /// A trait for instances of a trading client, which allows operations with the underlying trading broker.
 pub trait IronTradeClient {
+    /// Places an order. If successful returns the order id of the newly placed order.
+    fn place_order(&mut self, req: OrderRequest) -> impl Future<Output = Result<OrderResponse>>;
+
     /// Places a buy order. If successful returns the order id of the newly placed order.
-    fn buy(
-        &mut self,
-        req: OrderRequestV1,
-    ) -> impl Future<Output = Result<OrderResponse>> + Send;
+    fn buy(&mut self, req: OrderRequestV1) -> impl Future<Output = Result<OrderResponse>> + Send;
 
     /// Places a sell order. If successful returns the order of the newly placed order.
-    fn sell(
-        &mut self,
-        req: OrderRequestV1,
-    ) -> impl Future<Output = Result<OrderResponse>> + Send;
+    fn sell(&mut self, req: OrderRequestV1) -> impl Future<Output = Result<OrderResponse>> + Send;
 
-    /// Returns all placed orders regardless of status.
-    fn get_orders(&self) -> impl Future<Output = Result<GetOrdersResponseV1>> + Send;
+    /// Returns all placed orders.
+    fn get_orders_v1(&self) -> impl Future<Output = Result<GetOrdersResponseV1>> + Send;
+
+    /// Returns all placed orders.
+    fn get_orders(&self) -> impl Future<Output = Result<GetOrdersResponse>> + Send;
 
     /// Returns the current cash balance, more specifically the balance of the currency
     /// that is not tied to any order or open position.
