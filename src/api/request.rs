8ef2d548651f7d644d608b735adc0dbc35b36c2c
Agostinho Junior <junioraw5732@gmail.com>
2025-11-23 18:14:40 +0000

    Remove OrderRequestV1

--- src/api/request.rs
+++ src/api/request.rs
@@ -3,12 +3,6 @@
 use num_decimal::Num;
 use crate::api::common::{Amount, AssetPair, OrderSide};
 
-pub struct OrderRequestV1 {
-    pub asset_pair: AssetPair,
-    pub amount: Amount,
-    pub limit_price: Option<Num>
-}
-
 pub struct OrderRequest {
     pub asset_pair: AssetPair,
     pub amount: Amount,
