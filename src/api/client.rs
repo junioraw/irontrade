c25704a1f71874897d04deac4a8a0ac289c000de
Agostinho Junior <junioraw5732@gmail.com>
2025-11-23 18:14:10 +0000

    Remove v1 order methods and structs

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use crate::api::request::{OrderRequest, OrderRequestV1};
+use crate::api::request::OrderRequest;
 use crate::api::response::{
-    GetCashResponse, GetOpenPositionResponse, GetOrdersResponse, GetOrdersResponseV1, OrderResponse,
+    GetCashResponse, GetOpenPositionResponse, GetOrdersResponse, OrderResponse,
 };
 use anyhow::Result;
 
@@ -10,15 +10,6 @@ use anyhow::Result;
 pub trait IronTradeClient {
     /// Places an order. If successful returns the order id of the newly placed order.
     fn place_order(&mut self, req: OrderRequest) -> impl Future<Output = Result<OrderResponse>>;
-
-    /// Places a buy order. If successful returns the order id of the newly placed order.
-    fn buy(&mut self, req: OrderRequestV1) -> impl Future<Output = Result<OrderResponse>> + Send;
-
-    /// Places a sell order. If successful returns the order of the newly placed order.
-    fn sell(&mut self, req: OrderRequestV1) -> impl Future<Output = Result<OrderResponse>> + Send;
-
-    /// Returns all placed orders.
-    fn get_orders_v1(&self) -> impl Future<Output = Result<GetOrdersResponseV1>> + Send;
 
     /// Returns all placed orders.
     fn get_orders(&self) -> impl Future<Output = Result<GetOrdersResponse>> + Send;
