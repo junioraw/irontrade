5b89cfad353157e38efdfa729f57ec1aac1cb7ab
Agostinho Junior <junioraw5732@gmail.com>
2025-11-30 18:51:12 +0000

    Reformat

--- src/api/market.rs
+++ src/api/market.rs
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use anyhow::Result;
 use crate::api::common::{AssetPair, Bar, Quote};
+use anyhow::Result;
 
 pub trait Market {
-
-    fn get_latest_minute_bar(&self, asset_pair: &AssetPair) -> impl Future<Output = Result<Bar>> + Send;
-
-    fn get_latest_quotes(&self, asset_pair: &AssetPair) -> impl Future<Output = Result<Quote>> + Send;
+    fn get_latest_minute_bar(
+        &self,
+        asset_pair: &AssetPair,
+    ) -> impl Future<Output = Result<Bar>> + Send;
+    fn get_latest_quotes(
+        &self,
+        asset_pair: &AssetPair,
+    ) -> impl Future<Output = Result<Quote>> + Send;
 }
