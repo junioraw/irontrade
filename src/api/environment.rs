f8bcc3bba8a97b406251a810c03ff189a42a3a8f
Agostinho Junior <junioraw5732@gmail.com>
2025-12-07 13:58:49 +0000

    Environment: Client + Market
    Instead of a method per trait

--- src/api/environment.rs
+++ src/api/environment.rs
@@ -3,7 +3,4 @@
 use crate::api::client::Client;
 use crate::api::market::Market;
 
-pub trait Environment {
-    fn get_client() -> impl Client;
-    fn get_market() -> impl Market;
-}
+pub trait Environment: Client + Market {}
