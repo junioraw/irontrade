7bd853c7ee1ed94a525f037612ec3d0596400d4b
Agostinho Junior <junioraw5732@gmail.com>
2025-12-08 00:06:42 +0000

    Add ManualClock

--- src/simulated/time.rs
+++ src/simulated/time.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 use std::ops::Mul;
 
 pub trait Clock {
@@ -8,17 +8,17 @@ pub trait Clock {
 }
 
 pub struct SpeedClock {
-    last_artificial_time: DateTime<Utc>,
-    last_actual_time: DateTime<Utc>,
-    speed_multiplier: i32,
+    artificial_time: DateTime<Utc>,
+    actual_time: DateTime<Utc>,
+    time_speed_multiplier: i32,
 }
 
 impl SpeedClock {
-    pub fn start(start_time: DateTime<Utc>, speed_multiplier: i32) -> Self {
+    pub fn start(start_time: DateTime<Utc>, time_speed_multiplier: i32) -> Self {
         SpeedClock {
-            last_artificial_time: start_time,
-            last_actual_time: Utc::now(),
-            speed_multiplier,
+            artificial_time: start_time,
+            actual_time: Utc::now(),
+            time_speed_multiplier,
         }
     }
 }
@@ -26,13 +26,33 @@ impl SpeedClock {
 impl Clock for SpeedClock {
     fn now(&mut self) -> DateTime<Utc> {
         let last_actual_time = Utc::now();
-        let time_delta = last_actual_time - self.last_actual_time;
-        self.last_actual_time = last_actual_time;
+        let time_delta = last_actual_time - self.actual_time;
+        self.actual_time = last_actual_time;
 
         let last_artificial_time =
-            self.last_artificial_time + time_delta.mul(self.speed_multiplier);
-        self.last_artificial_time = last_artificial_time;
+            self.artificial_time + time_delta.mul(self.time_speed_multiplier);
+        self.artificial_time = last_artificial_time;
 
         last_artificial_time
+    }
+}
+
+pub struct ManualClock {
+    artificial_time: DateTime<Utc>,
+}
+
+impl ManualClock {
+    pub fn new(start_time: DateTime<Utc>) -> Self {
+        ManualClock { artificial_time: start_time }
+    }
+
+    pub fn advance(&mut self, duration: Duration) {
+        self.artificial_time = self.artificial_time + duration;
+    }
+}
+
+impl Clock for ManualClock {
+    fn now(&mut self) -> DateTime<Utc> {
+        self.artificial_time
     }
 }
