d914c7ef50c067c57baf9cdee08499ac4eb56f89
Agostinho Junior <junioraw5732@gmail.com>
2025-12-23 20:28:32 +0000

    Make api namespaces more concise for mods under `crate::api` and `crate::simulated`
    E.g. `crate::api::client::Client` becomes `crate::api::Client`

--- src/simulated.rs
+++ src/simulated.rs
@@ -1,9 +1,19 @@
 // Copyright (C) 2025 Agostinho Junior
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pub mod broker;
-pub mod client;
-pub mod environment;
+pub use broker::SimulatedBrokerBuilder;
+pub use broker::SimulatedBroker;
+mod broker;
+
+pub use client::SimulatedClient;
+mod client;
+
+pub use environment::SimulatedEnvironment;
+pub use environment::SimulatedEnvironmentBuilder;
+mod environment;
+
 pub mod time;
 pub mod data;
-pub mod context;
+
+pub use context::SimulatedContext; 
+mod context;
